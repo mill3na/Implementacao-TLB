c281782a78772125104be0a63aeb07e81fa9c813
mill3na <58698807+mill3na@users.noreply.github.com>
2021-06-29 17:51:10 -0300

    Create TesteTraces.py
    Função com leitura e conversão de hexadecimal para inteiro dos traces de memória de uma aplicação real retiradas do site: http://traces.cs.umass.edu/index.php/CpuMem/CpuMem

---
+++ TesteTraces.py
@@ -0,0 +1,36 @@
+def ler_arquivo(traces_hexadecimal, traces_inteiros):
+    try:
+        a = open(traces_hexadecimal, 'r')
+    except:
+        print("Não sei cadê o arquivo.")
+    else:
+        contador = 0
+        posicoes_valores = {"posição": 0, "valor": 0}
+        for linha in a:
+            dado_inteiro = []
+            dado = linha.split()
+            espaco = [" "]
+            for i in linha:
+                if i in espaco:
+                    contador += 1
+                    if contador == 3:
+                        aux = int(dado[3], 16)
+                        dado_inteiro.append(aux)
+                        contador = 0
+                        #print(aux)
+                        with open(traces_inteiros, 'at') as b:
+                            b.write(f'{aux}\n')
+
+
+def criar_arquivo(nome):
+    try:
+        a = open(nome, 'a')
+        a.close()
+    except:
+        print("Houve um erro na criação do arquivo.")
+
+
+arq = "traces.txt"
+arq_inteiros = "tracesInteiros.txt"
+ler_arquivo(arq, arq_inteiros)
+criar_arquivo("tracesInteiros.txt")
