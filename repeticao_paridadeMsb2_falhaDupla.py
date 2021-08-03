ad6114ef0cf466ddce50586ca73aa3037b2ebca2
mill3na <58698807+mill3na@users.noreply.github.com>
2021-08-03 15:23:44 -0300

    script de repetição para paridade msb2 e falha dupla

---
+++ repeticao_paridadeMsb2_falhaDupla.py
@@ -0,0 +1,54 @@
+# Este script roda o simulador.py e seta os parâmetros para paridade msb2 e falha dupla
+import simulador
+import random
+from simulador import executaSimulador
+
+
+# Declaração dos parâmetros fixos
+total_cache = 8
+arquivo_acesso = "traces.txt"
+debug = 0
+
+# Variável de controle do for que imprime a iteração atual
+i = 0
+
+def quantidade_linhas_arquivo():
+    a = open(arquivo_acesso, 'r')
+    contador = 0
+    for linha in a:
+        contador += 1
+
+    return contador
+
+
+# salva o resultado na variável senão acaba chamando a função a cada ciclo do for
+linhas = quantidade_linhas_arquivo()
+
+# Salva o resultado final de falsos positivos
+fp_final = 0
+
+# Seta os parâmetros
+# Quando for testar, copia e cola alterando o tipo de código para cada tipo de falha
+# Seta no range do for a quantidade de vezes que vai rodar para cada teste
+
+
+for i in range(0, 10):
+
+
+    codigo = "PARIDADE_2MSB"
+    endereco_falha = random.randint(0, linhas)
+    linha_tlb_falha = random.randint(1, 7)
+    bit_falho = random.randint(1, 31)
+    tipo_falhas_inseridas = "FALHA_DUPLA"
+
+    # chama o simulador.py, executa e contabiliza o retorno
+    simulador.executaSimulador(total_cache, arquivo_acesso, debug, codigo, endereco_falha, linha_tlb_falha, bit_falho,
+                               tipo_falhas_inseridas)
+    fp = executaSimulador(total_cache, arquivo_acesso, debug, codigo, endereco_falha, linha_tlb_falha, bit_falho,
+                          tipo_falhas_inseridas)
+    if fp == 1:
+        fp_final += 1
+
+    print("— "*20)
+    print(f'\nIteração: {i+1}. Número de falsos positivos: {fp_final}.\n')
+print(f'\033[31m\nNúmero de iterações {i + 1}. \nFalsos positivos ao longo da simulação: {fp_final}.\033[m')
