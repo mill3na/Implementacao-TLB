897671394effb75fc95a65fc212e92f28b767d99
Walber Florêncio <59696629+walberfa@users.noreply.github.com>
2021-08-05 21:51:57 -0300

    Script de repetição com falha tripla e paridade 2MSB

---
+++ repeticao_2msb_tripla.py
@@ -0,0 +1,42 @@
+# Este script roda o main.py e seta os parâmetros.
+
+import main
+import random
+from main import executaSimulador
+
+#Declaração dos parâmetros fixos
+total_cache = 8
+arquivo_acesso = "traces.txt"
+debug = 0
+
+def quantidade_linhas_arquivo():
+    a = open(arquivo_acesso, 'r')
+    contador  = 0
+    for linha in a:
+        contador += 1
+    #print(contador)
+    return contador
+
+#salva o resultado na variável senão acaba chamando a função a cada ciclo do for
+linhas = quantidade_linhas_arquivo()
+
+
+# Seta os parâmetros
+# Quando for testar, copia e cola alterando o tipo de código para cada tipo de falha
+# Seta no range do for a quantidade de vezes que vai rodar para cada teste
+
+for i in range(0,1000):
+
+    codigo = "PARIDADE_2MSB"
+    endereco_falha = random.randint(0, linhas)
+    linha_tlb_falha = random.randint(1,7)
+    bit_falho = random.randint(1, 31)
+    tipo_falhas_inseridas = "FALHA_TRIPLA"
+
+    # chama o main.py, executa e contabiliza o retorno
+    
+    main.executaSimulador(total_cache, arquivo_acesso, debug, codigo, endereco_falha, linha_tlb_falha, bit_falho, tipo_falhas_inseridas)
+    print("iteração:",i+1)
+    #print("codigo: paridade 2MSB")
+    #print("falha tripla")
+    print('-' * 5)
